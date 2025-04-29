82a6eba6254cc33247da222aaf82b32ce4850f6a
Sidduzzzup <nova72949@gmail.com>
2025-04-29 22:06:34 +0530

    Fixed routing issue with version update 2.0

--- src/stores/components/PsOffers.jsx
+++ src/stores/components/PsOffers.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PsOffers = () => {
-    const imagePath = '/assets/ps5.jpg';  // Define the image path
+    const imagePath = '/ps5.jpg';  // Define the image path
   
     //console.log(`Image path: ${imagePath}`);  // Log the image path
   return (
